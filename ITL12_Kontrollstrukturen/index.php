f93989a7e1cc9cb46c6e1662ea5f3ecacde60041
Jenby32 <kevinkozar2005@gmail.com>
2024-02-12 09:34:44 +0100

    cleaned code and added comments

--- ITL12_Kontrollstrukturen/index.php
+++ ITL12_Kontrollstrukturen/index.php
@@ -1,26 +1,34 @@
 <?php
+
+    // define function to get all primenumbers
     function getPrimeNumbers($value) {
-        $flag = false;
-        $array = "";
+        // first iteration to iterate through every number till passed argument value
         for ($i = 2; $i < $value; $i++) {
+            // create kind of a flag
             $temp = 0;
+            // implement function to determine whether the number is prime or not
             for ($j = 2; $j <= $i/2; $j++) {
                 if($i % $j == 0) {
                     $temp++;
-                    $flag = true;
                 }
             }
 
+            // if flag is still zero then number is prime
             if($temp == 0) {
                 echo "$i<br/>";
             }
         }
     }
 
+    // function for the log excercise
     function logExcercise() {
+        // iterate from 1 to 100
         for ($i = 1; $i <= 100; $i++) {
+            // create a str for the stars
             $starStr = "";
+            // a variable which holds the nummber -> log(i) times the factor
             $repeat = 10*log($i);
+            // iterate from 1 to the repeat number and add n times *
             for($j = 1; $j < $repeat; $j++) {
                 $starStr .= "*";
             }
@@ -28,13 +36,13 @@
         }
     }
 
+    // every post request - button click - the 2 functions gets calles
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $zahl1 = $_POST["zahl1"];
         getPrimeNumbers($zahl1);
         logExcercise();
     }
 ?>
-
 <form action="" method="post">
     <label for="zahl1">Zahl:</label><br>
     <input type="text" id="zahl1" name="zahl1"><br>
